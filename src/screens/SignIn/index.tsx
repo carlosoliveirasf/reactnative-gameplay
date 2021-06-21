0a9b281c75b622771964c330e97b6cbc749ac8f1
Carlos Oliveira <carlostifelipe@gmail.com>
2021-06-21 21:11:11 +0100

    first update, adding src

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StatusBar,
+  Button
+} from 'react-native';
+
+import { ButtonIcon } from '../../components/ButtonIcon';
+import IllustrationImg from '../../assets/illustration.png';
+import { styles } from './styles';
+
+export function SignIn() {
+
+  const [text, setText] = useState();
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <Image source={IllustrationImg} style={styles.image} resizeMode="stretch" />
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {`\n`}
+          suas jogatinas {`\n`}
+          facilmente
+        </Text>
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games{`\n`}
+          favoritos com seus amigos
+        </Text>
+        <ButtonIcon title="Entrar com Discord" />
+      </View>
+    </View>)
+}
